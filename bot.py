b28ab5d71b5824dbbda71a2e7a52401ef7a40d9d
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-08 22:04:20 +1100

    changed choice to sample for player selection

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ while True:
    
     print("Getting the tweet:")
     player_1 = get_unique_name
-    print(player_1 + '\n')
+    print(player_1)
     player_2 = get_unique_name(player_1)
     print(player_2)
     media_url_1 = get_media_url(player_1)
