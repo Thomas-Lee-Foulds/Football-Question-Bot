22458929675d753d01195aaba6f1bd40b9332e43
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-08 22:30:31 +1100

    changed choice to sample for player selection

--- player_selector.py
+++ player_selector.py
@@ -2,5 +2,5 @@ from random import sample
 
 def get_unique_name():
     players = set(line.strip() for line in open('players.txt'))
-    rand_name = sample(players,1)[0]
+    rand_name = sample(players,1)
     return rand_name
