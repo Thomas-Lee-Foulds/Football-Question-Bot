1f037cda7996eac09755aa8ce767032f14ee4fef
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-08 23:25:06 +1100

    changed choice to sample for player selection

--- image_getter.py
+++ image_getter.py
@@ -10,6 +10,6 @@ def get_media_url(player_name):
                  "size": "medium", 
                  "aspect_ratio": "panoramic"} 
     path = response.download(arguments)
-    path = path[1]
+
     print(path)
     return path
