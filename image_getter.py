3cb979ad7879e270a993c5ae07c3f1e4fbc7ea7c
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-08 22:46:45 +1100

    changed choice to sample for player selection

--- image_getter.py
+++ image_getter.py
@@ -1,11 +1,6 @@
 from google_images_download import google_images_download 
-import sys
-
 
 def get_media_url(player_name):
-    orig_stdout = sys.stdout
-    f = open('URLS.txt', 'w')
-    sys.stdout = f
     response = google_images_download.googleimagesdownload()  
     query = str(player_name) + " " + "football"
     arguments = {"keywords": query, 
@@ -14,16 +9,5 @@ def get_media_url(player_name):
                  "print_urls":True, 
                  "size": "medium", 
                  "aspect_ratio": "panoramic"} 
-    paths = response.download(arguments)
-    sys.stdout = orig_stdout
-    
-
-    with open('URLS.txt') as f:
-        content = f.readlines()
-    f.close()
-
-    urls = []
-    for j in range(len(content)):
-        if content[j][:9] == 'Completed':
-            urls.append(content[j-1][11:-1]) 
-    return urls[0]
+    path = response.download(arguments)
+    return google_images_download.print_urls
