59688651c0ae2141c1fcb3f0654821d76e293309
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-09 13:33:37 +1100

    changed choice to sample for player selection

--- tweet_former.py
+++ tweet_former.py
@@ -1,3 +1,3 @@
 def generate_tweet(player_1,player_2):
-    tweet = ('Who is better?, ' + str(player_1) + ' or ' + str(player_2) + '\nComment or Quote Retweet Below!')
+    tweet = ('Who is better?, ' + str(player_1) + ' or ' + str(player_2) + '\nComment or Quote Retweet Below!\n#Football')
     return tweet
