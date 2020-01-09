348d7f921d2441f1ca4c0f6adf00fe8db37bc1ca
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-09 19:26:39 +1100

    added full automation

--- clock.py
+++ clock.py
@@ -2,7 +2,8 @@ from bot import tweet
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler()
+sched.start() 
 @sched.scheduled_job('interval', minutes=60)
 def timed_job():
     tweet()
-sched.start()  
+ 
