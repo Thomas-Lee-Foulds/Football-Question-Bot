2fecd71f0b9823feb1bbe699b72472a00ce6f74a
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-09 12:10:02 +1100

    changed choice to sample for player selection

--- image_getter.py
+++ image_getter.py
@@ -4,7 +4,8 @@ def get_media_url(player_name, google_api, search_engine_id):
     resource = build("customsearch", "v1", developerKey= google_api).cse()
     query = str(player_name) + " " + "football"
     result = resource.list(q=query, cx = search_engine_id, searchType='image', imgSize='medium', num = 1)
-    for item in result['items']:
-        item_response = item.json()
-        
-    return  item_response["link"]
+    result_search = result.json()
+    print(result_search)
+    for item in result_search['items']:
+        url = item["link"]   
+    return  url
