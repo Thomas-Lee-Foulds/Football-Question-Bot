1b3c946bf952d2375a2275694b6aca763c9edc3b
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-10 17:21:48 +1100

    added multiple files and support for them

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from os import environ
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.run(host='0.0.0.0', port=environ.get('PORT'))
+app.run(host='0.0.0.0', port='1337')
 @app.route('/')
 @app.route('/index')
 def index():
--- player_selector.py
+++ player_selector.py
@@ -1,18 +1,23 @@
 import random
 
 def get_names():
-    
-    chosen = random.randint(1,5)
-    if chosen == 1:
-        players = set(line.strip() for line in open('Attackers.txt'))
-        players_chosen = random.sample(players,2)
-    elif chosen == 2:
-        players = set(line.strip() for line in open('Midfielders.txt'))
-        players_chosen = random.sample(players,2)
-    elif chosen == 3:
-        players = set(line.strip() for line in open('Defenders.txt'))
-        players_chosen = random.sample(players,2)
-    elif chosen == 4:
-        players = set(line.strip() for line in open('Goalkeepers.txt'))
-        players_chosen = random.sample(players,2)
+    while True:  
+        chosen = random.randint(1,5)
+        if chosen == 1:
+            players = set(line.strip() for line in open('Attackers.txt'))
+            players_chosen = random.sample(players,2)
+            break
+        elif chosen == 2:
+            players = set(line.strip() for line in open('Midfielders.txt'))
+            players_chosen = random.sample(players,2)
+            break
+        elif chosen == 3:
+            players = set(line.strip() for line in open('Defenders.txt'))
+            players_chosen = random.sample(players,2)
+            break
+        elif chosen == 4:
+            players = set(line.strip() for line in open('Goalkeepers.txt'))
+            players_chosen = random.sample(players,2)
+            break
+                
     return players_chosen
