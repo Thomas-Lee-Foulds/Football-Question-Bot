5596f833d1a3ae9673e9e44f4c376a8923f74a05
Thomas L. Foulds <fouldsthomas@gmail.com>
2020-01-10 16:56:31 +1100

    added multiple files and support for them

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
 from os import environ
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.run(host='0.0.0.0', port=environ.get('PORT'))
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
